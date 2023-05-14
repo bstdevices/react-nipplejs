b97c0a0d5e7974623a410c7566eb4238c5c7d241
Ola Malmgren <malmgrenola@gmail.com>
2023-05-14 09:19:56 +0200

    export types

--- packages/react-nipplejs/src/index.tsx
+++ packages/react-nipplejs/src/index.tsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as React from 'react';
 import { Nipple, NippleProps } from './Nipple';
+export { type JoystickManagerOptions, type EventData, type JoystickOutputData } from 'nipplejs';
 export { type NippleProps };
 export { Nipple };
 export default Nipple;
